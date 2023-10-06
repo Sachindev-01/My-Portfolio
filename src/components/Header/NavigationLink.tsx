35aa973e5dc67ef4bbe4b0f84d57c3e3bf6c8dd5
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-10-06 06:02:36 +0500

    add some changes in navigation links

--- src/components/Header/NavigationLink.tsx
+++ src/components/Header/NavigationLink.tsx
@@ -25,7 +25,7 @@ const NavigationLink = ({ urlName, url, handleToggle }: NavigationProps) => {
     <NavLink
       to={url}
       onClick={handleOnClick}
-      className={`w-[100px] text-center font-font-calibre ml-4 px-4 py-2 lg:p-4 text-[#fff] text-lg capitalize lg:text-xl  hover:bg-[#113b5e] transition duration-150 ease-in-out border-t-[.5px] border-[#3f9eee] lg:border-t-[0px] lg:ml-0 lg:text-center lg:hover:bg-transparent lg:hover:border-b-[.5px] lg:border-[#3f9eee] lg:w-auto lg:hover:text-[#3f9eee] ${
+      className={`w-[100px] text-center font-font-calibre mx-4 px-4 py-2 lg:p-4 text-[#fff] text-lg capitalize lg:text-xl  hover:bg-[#113b5e] transition duration-150 ease-in-out border-t-[.5px] border-[#3f9eee] lg:border-t-[0px] lg:ml-0 lg:text-center lg:hover:bg-transparent lg:hover:border-b-[.5px] lg:border-[#3f9eee] lg:w-auto lg:hover:text-[#3f9eee] ${
         pathMatchRoute(url) && "lg:border-b-2 lg:border-[#3f9eee]"
       }`}
     >
