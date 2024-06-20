7cc6cdf21a005fb3fca7f4621258954bfc30fa73
sachinDev07 <sachinkrj077@gmail.com>
2024-06-20 15:49:13 +0530

    Added resume url link

--- src/components/DownloadButton/DownloadButton.tsx
+++ src/components/DownloadButton/DownloadButton.tsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
+
+const resumeUrl = import.meta.env.VITE_RESUME_URL as string;
+
 const DownloadButton = () => {
   return (
-    <a
-      download="SachinResume"
-      href="/src/assets/SachinResume.pdf"
+    <Link
+      to={resumeUrl}
       target="_blank"
       aria-label="Download My Resume"
       className="mt-6 border-2 border-blue-200 py-1 px-3 lg:py-2 lg:px-4 inline-block hover:bg-[#3f9eee] hover:border-[#3f9eee] hover:text-black hover:scale-105 transition duration-150 ease-in-out"
     >
-      <button className="lg:text-lg">Download Resume</button>
-    </a>
+      <button className="lg:text-lg px-2 -tracking-tighter">RESUME</button>
+    </Link>
   );
 };
 
